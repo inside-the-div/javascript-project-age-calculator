22c40b34d911a3137a549824d632438057a5b880
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-02-08 12:36:21 +0600

    empty check

--- JS/main.js
+++ JS/main.js
@@ -3,34 +3,19 @@
 function CalculateResult(){
     
     var fromDate = document.getElementById("birthDate");
-    var toDate = document.getElementById("toDate");
-    var isValid = true;
-    if(isValid)
-    {  var fromDateValue = fromDate.value;      
-        if(fromDateValue == "")
-        {
-            alert("Select Your Birthday.");
-            fromDate.focus();
-            fromDate.style.border = "2px solid red";
-            isValid = false;
-        }
-    }
-    
-    if(isValid)
+    var toDate = document.getElementById("toDate");    
+    var fromDateValue = fromDate.value;
+    var toDateValue = toDate.value;
+
+    if(fromDateValue == "")
     {
-        var toDateValue = toDate.value;
-        if(toDateValue == "")
-        {
-            alert("Select The To Date.");
-            toDate.focus();
-            toDate.style.border = "2px solid red";
-            isValid = false;
-        }
-    }
-    if(isValid)
+        alert("Select Your Birthday.");
+    }    
+    else if(toDateValue == "")
     {
-        // calculation method
+        alert("Select The To Date.");
     }
+
 }
 
 function ResetFunction(){
@@ -38,6 +23,4 @@ function ResetFunction(){
     var toDate = document.getElementById("toDate");
     fromDate.value = "";
     toDate.value = "";
-    toDate.style.border = "2px solid white";
-    fromDate.style.border = "2px solid white";
 }
