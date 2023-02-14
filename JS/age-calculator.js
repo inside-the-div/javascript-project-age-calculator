0accb1621756f4e45bed949234cfb8bbc812d80f
kamrul_hasan_10 <rkamrulhasan7@gmail.com>
2023-02-14 18:27:31 +0600

    age calculator error found

--- JS/age-calculator.js
+++ JS/age-calculator.js
@@ -14,107 +14,10 @@ function CalculateResult()
     
     if(ProjectFormValidate())
     {
-    //     var NoDayRegularMonth = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-    //     var NoDayLeapMonth = [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
-
-    //     birthDate = new Date(fromDate);        
-    //     currentDate = new Date(toDate);
-
-    //     var BirthYear = birthDate.getFullYear();
-    //     var CurrentYear = currentDate.getFullYear();
-    //     var BirthMonth = birthDate.getMonth() + 1; 
-    //     var CurrentMonth = currentDate.getMonth() + 1;
-    //     var BirthDateDay = birthDate.getDate();
-    //     var CurrentDay = currentDate.getDate();
-        
-    //     if(count == 0)
-    //     {
-    //         document.getElementById("OutputInfo").style = "display: none";
-    //         document.getElementById("OutputResult").style = "display: flex";
-    //         count++;
-    //     }
-        
-    //     if (BirthDateDay > CurrentDay)
-    //     {
-    //         if (LeapYear(BirthYear))
-    //         {
-    //             CurrentDay = CurrentDay + NoDayLeapMonth[CurrentMonth - 1];
-    //             CurrentMonth = CurrentMonth - 1;
-    //         }
-    //         else
-    //         {
-    //             CurrentDay = CurrentDay + NoDayRegularMonth[CurrentMonth - 1];
-    //             CurrentMonth = CurrentMonth - 1;
-    //         }
-    //     }
-
-    //     var  LeapYearCount = 0;
-    //     for (var i = BirthYear; i <= CurrentYear; i++)
-    //     {
-    //         if (LeapYear(i))
-    //         {
-    //             if(BirthYear == i)
-    //             {
-    //                 if (BirthMonth <= 2)
-    //                 {
-    //                     LeapYearCount++;
-    //                 }
-    //             }
-    //             else if (CurrentYear == i)
-    //             {
-    //                 if (CurrentMonth >= 3)
-    //                 {
-    //                     LeapYearCount++;
-    //                 }
-    //             }
-    //             else
-    //             {
-    //                 LeapYearCount++;
-    //             }                                        
-    //         }                    
-    //     }
-
-    //     if (BirthMonth > CurrentMonth)
-    //     {
-    //         CurrentYear = CurrentYear - 1;
-    //         CurrentMonth = CurrentMonth + 12;
-    //     }
-
-    //     var CalculatedDay = Math.trunc(CurrentDay - BirthDateDay);
-    //     var CalculatedMonth = Math.trunc(CurrentMonth - BirthMonth);
-    //     var CalculatedYear = Math.trunc(CurrentYear - BirthYear);
-
-    //     if(CalculatedYear <= 1)
-    //     {
-    //         resultYear.innerHTML = CalculatedYear + " Year";
-    //     }
-    //     else
-    //     {
-    //         resultYear.innerHTML = CalculatedYear + " Years";
-    //     }
-
-    //     if(CalculatedMonth <= 1)
-    //     {
-    //         resultMonth.innerHTML = CalculatedMonth + " Month";
-    //     }
-    //     else
-    //     {
-    //         resultMonth.innerHTML = CalculatedMonth + " Months";
-    //     }
-
-    //     if(CalculatedDay <= 1)
-    //     {
-    //         resultDay.innerHTML = CalculatedDay + " day";
-    //     }
-    //     else
-    //     {
-    //         resultDay.innerHTML = CalculatedDay + " days";
-    //     }     
-    // }
-    const birthDate = new Date(1996, 5, 27);
-const currentDate = new Date();
-const age = calculateAccurateAge(birthDate, currentDate);
-console.log(`Age: ${age.years} years, ${age.months} months, ${age.days} days, Total Days: ${age.totalDays}, Leap Years: ${age.leapYears}`);
+      const birthDate = new Date(1996, 5, 27);
+      const currentDate = new Date();
+      const age = calculateAccurateAge(birthDate, currentDate);
+    }   
 
 }
 
@@ -127,7 +30,7 @@ function calculateAccurateAge(birthDate, currentDate) {
     let leapYears = 0;
   
     for (let i = birthDate.getUTCFullYear(); i <= currentDate.getUTCFullYear(); i++) {
-      if (isLeapYear(i)) {
+      if (LeapYear(i)) {
         leapYears++;
       }
     }
@@ -142,11 +45,6 @@ function calculateAccurateAge(birthDate, currentDate) {
     };
   }
   
-  function isLeapYear(year) {
-    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
-  }
-  
-
 function ResetAgeCalculator()
 {
     document.getElementById("birthDate").value = "";;
